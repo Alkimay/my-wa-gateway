2d4879ca31140aeaba48080b84dea1cb06761a7b
kimax <solvitcompany@gmail.com>
2024-10-22 19:31:33 +0300

    Fix

--- app/controllers/message_controller.js
+++ app/controllers/message_controller.js
@@ -8,12 +8,14 @@ const os = require('os');
 
 exports.sendMessage = async (req, res, next) => {
   try {
-    let to = req.body.to || req.query.to;
-    let text = req.body.text || req.query.text;
-    let isGroup = req.body.isGroup || req.query.isGroup;
 
+    let to = req.body.number || req.query.number;
+    let text = req.body.message || req.query.message;
+    let isGroup = req.body.isGroup || req.query.isGroup;
+    let access_token = req.body.access_token || req.query.access_token;
+    if(access_token !== process.env.KEY) throw new ValidationError("Access Token Invalid")
     const sessionId =
-      req.body.session || req.query.session || req.headers.session;
+      req.body.instance_id || req.query.instance_id || req.headers.instance_id;
 
     if (!to || !text) throw new ValidationError("Missing Parameters");
 
@@ -71,13 +73,15 @@ exports.readMessage = async (req, res, next) => {
 };
 exports.sendMessageFile = async (req, res, next) => {
   try {
-    let to = req.body.to || req.query.to;
-    let text = req.body.text || req.query.text;
-    let isGroup = req.body.isGroup || req.query.isGroup;
-    let fileWebHttp = req.body.file || req.query.file;
-    let filename = req.body.filename || req.query.filename;
 
-    const sessionId = req.body.session || req.query.session || req.headers.session;
+    let to = req.body.number || req.query.number;
+    let text = req.body.message || req.query.message;
+    let isGroup = req.body.isGroup || req.query.isGroup;
+    let fileWebHttp = req.body.media_url || req.query.media_url;
+    let filename = req.body.filename || req.query.filename;
+    let access_token = req.body.access_token || req.query.access_token;
+    if(access_token !== process.env.KEY) throw new ValidationError("Access Token Invalid")
+    const sessionId = req.body.instance_id || req.query.instance_id || req.headers.instance_id;
 
     if (!to || !fileWebHttp) throw new ValidationError("Missing Parameters");
     if (!sessionId) throw new ValidationError("Session Not Found");
